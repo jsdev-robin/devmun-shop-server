fbe7ebf5c0d5f930fce068eba164269f83ac02ea
Md Robin <jsdev.robin@gmail.com>
2025-07-25 19:41:28 +0600

    Fix Config test file

--- libs/config/src/lib/config.spec.ts
+++ libs/config/src/lib/config.spec.ts
@@ -1,7 +1,13 @@
 import { config } from './config.js';
 
 describe('config', () => {
-  it('should work', () => {
-    expect(config()).toEqual('config');
+  it('should contain the expected configuration properties', () => {
+    expect(config).toHaveProperty('NODE_ENV');
+    expect(config).toHaveProperty('PORT');
+    expect(config).toHaveProperty('DB');
+    expect(config).toHaveProperty('ISPRODUCTION');
+
+    expect(typeof config.PORT).toBe('string');
+    expect(typeof config.ISPRODUCTION).toBe('boolean');
   });
 });
