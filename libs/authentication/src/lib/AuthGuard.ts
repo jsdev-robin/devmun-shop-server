a29cbe4dad9e57f0d6b6a77bbb2290c20db7bdf3
Md Robin <jsdev.robin@gmail.com>
2025-07-26 14:45:45 +0600

    feat(auth): implement validateToken middleware for verifying access tokens and signature integrity

--- libs/authentication/src/lib/AuthGuard.ts
+++ libs/authentication/src/lib/AuthGuard.ts
@@ -1,6 +1,8 @@
-import { config } from '@server/config';
+import { config, nodeClient } from '@server/config';
+import { ApiError } from '@server/middleware';
 import { IUser } from '@server/models';
-import { catchAsync } from '@server/utils';
+import { Crypto } from '@server/security';
+import { catchAsync, HttpStatusCode } from '@server/utils';
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { Model } from 'mongoose';
@@ -45,6 +47,48 @@ export class AuthGuard<T extends IUser> extends AuthEngine {
         return this.sessionUnauthorized(res, next);
       }
 
+      next();
+    }
+  );
+
+  public requireAuth = catchAsync(
+    async (
+      req: Request<unknown, unknown, unknown> & {
+        userId?: string | undefined;
+        accessToken?: string | undefined;
+      },
+      res: Response,
+      next: NextFunction
+    ): Promise<void> => {
+      // Get credentials from request
+      const { userId, accessToken } = req;
+
+      // Query session and user data from Redis
+      const p = nodeClient.multi();
+
+      p.SISMEMBER(`${userId}:session`, Crypto.hmac(String(accessToken)));
+      p.json.GET(`${userId}`);
+
+      const [sessionToken, cachedUser] = await p.exec();
+
+      // Invalidate if session/user not found
+      if (!sessionToken || !cachedUser) {
+        return this.sessionUnauthorized(res, next);
+      }
+
+      // Resolve user from Redis or fallback to database
+      const user = cachedUser || (await this.model.findById(userId).exec());
+
+      if (!user) {
+        return next(
+          new ApiError(
+            "We couldn't find your account. Please contact support if you believe this is an error.",
+            HttpStatusCode.NOT_FOUND
+          )
+        );
+      }
+
+      req.self = user;
       next();
     }
   );
